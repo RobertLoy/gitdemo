e1e5c0786da4375fe8e46f7da8b2c32c98b9d4ca
RobertLoy <rob.loy@gmail.com>
2020-06-16 15:08:04 -0700

    initial commit

---
+++ src/git_gitdemo/Start.java
@@ -0,0 +1,34 @@
+package git_gitdemo;
+
+public class Start {
+
+	public static void main(String ...strings) {
+		funcPhilip();
+		funcSteve();
+		funcMara();
+		funcRob();
+		funcJames();
+		
+	}
+	
+	private static void funcPhilip() {
+		// 1) Create a syso with a motivational message
+	}
+	
+	private static void funcSteve() {
+		// Create a syso with a motivational message
+	}
+	
+	private static void funcMara() {
+		// Create a syso with a motivational message
+	}
+	
+	private static void funcRob() {
+		// Create a syso with a motivational message
+	}
+	
+	private static void funcJames() {
+		// Create a syso with a motivational message
+	}
+	
+}
